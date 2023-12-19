2c5d1708ddf1e73808e54f87073f4aa2c885ed56
tstrandvold <96109423+tstrandvold@users.noreply.github.com>
2023-12-19 15:07:07 +0000

    Add connection string and remove unused code

--- demo.py
+++ demo.py
@@ -7,16 +7,17 @@
 # last opp csv fil til blob storage
 
 
+connection_string = "DefaultEndpointsProtocol=https;AccountName=clausedemo;AccountKey=ooQFU1aU2ymE7rq2HsZ23LxpmBw6dKCiTJI2/gEwHHOt6n1xXjcVXuNgnGwQI8m3pouJfOS3fXZ+AStjK7UQA==;EndpointSuffix=core.windows.net"
+ 
 
 
-PROSJEKTID = row["PROSJEKTID"]
         ADRESSE = row["ADRESSE"]
         POSTNUMMER = row["POSTNUMMER"]
         headers = {
         'accept': 'application/json'
         }
         params=[('adressetekst',ADRESSE), ('postnummer',POSTNUMMER), ('side','0'), ('sokemodus','AND'), ('treffPerSide','1')]
-        url = "https://ws.geonorge.no/adresser/v1/sok" #?adressetekst=" + ADRESSE + "&postnummer=" + POSTNUMMER + "&side=0&sokemodus=AND&treffPerSide=1"
+        url = "https://ws.geonorge.no/adresser/v1/sok"
         response = requests.request("GET", url, params=params, headers=headers)
         # print(response.text)
         data = json.loads(response.text)
